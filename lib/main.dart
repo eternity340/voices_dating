d224c05ac235073a26ad2b59444efac1ad13daea
eternity340 <2698182174@qq.com>
2024-06-18 16:13:45 +0800

    app-route revise

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:first_app/routes/app_route.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'routes/app_route.dart';
+import 'package:get/get.dart' as getx;
 
 void main() {
   runApp(MyApp());
@@ -9,12 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return getx.GetMaterialApp(
       title: 'Email Verification',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/get_mail_code', // 设置初始路由
+      initialRoute: '/sign_in', // 设置初始路由为登录页面
       getPages: AppRoutes.routes, // 使用单独文件中的路由配置
     );
   }
--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -1,32 +1,42 @@
 import 'package:first_app/page/verify_success_page.dart';
-import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart' as getx;
 import '../page/get_email_code_page.dart';
+import '../pre_login/sign_in/sign_in_page.dart';
+import '../pre_login/sign_in/sign_in_provider.dart';
 import '../provider/get_email_code_provider.dart';
 import '../page/verify_email_page.dart';
+import '../provider/verify_email_provider.dart';
 
 
 class AppRoutes {
   static final routes = [
-    GetPage(
+    getx.GetPage(
+      name: '/sign_in',
+      page: () => SignInProvider(
+        child: SignInPage(),
+      ),
+    ),
+    getx.GetPage(
       name: '/get_mail_code',
       page: () => GetEmailCodeProvider(
         child: GetMailCodePage(),
       ),
     ),
-    GetPage(
+    getx.GetPage(
       name: '/verify_email',
       page: () {
-        final args = Get.arguments as Map<String, dynamic>;
+        final args = getx.Get.arguments as Map<String, dynamic>;
         return VerifyEmailPage(
           email: args['email'],
           verificationKey: args['verificationKey'],
         );
       },
     ),
-    GetPage(
+    getx.GetPage(
       name: '/verify_success',
       page: () {
-        final args = Get.arguments as Map<String, dynamic>;
+        final args = getx.Get.arguments as Map<String, dynamic>;
         return VerifySuccessPage(
           message: args['message'] ?? '',
         );
