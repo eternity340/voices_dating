338d7dfb16e423543c7eaa2b634369869769fa97
eternity340 <2698182174@qq.com>
2024-06-19 15:32:40 +0800

    main & normal  revise

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,48 @@
+import 'package:first_app/pre_login/welcome/welcome_page.dart';
 import 'package:first_app/routes/app_route.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart' as getx;
+import 'constants.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return getx.GetMaterialApp(
-      title: 'Email Verification',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            elevation: 0,
+            foregroundColor: Colors.white,
+            backgroundColor: kPrimaryColor,
+            shape: const StadiumBorder(),
+            maximumSize: const Size(double.infinity, 56),
+            minimumSize: const Size(double.infinity, 56),
+          ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
+          fillColor: kPrimaryLightColor,
+          iconColor: kPrimaryColor,
+          prefixIconColor: kPrimaryColor,
+          contentPadding: EdgeInsets.symmetric(
+              horizontal: defaultPadding, vertical: defaultPadding),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            borderSide: BorderSide.none,
+          ),
+        ),
       ),
-      initialRoute: '/sign_in', // 设置初始路由为登录页面
-      getPages: AppRoutes.routes, // 使用单独文件中的路由配置
+      initialRoute: '/welcome',
+      getPages: AppRoutes.routes,
+      home: const WelcomePage(), // 可选：initialRoute 已设置为 '/welcome'
     );
   }
 }
