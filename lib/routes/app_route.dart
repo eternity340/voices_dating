55bee500d73a19910fdb83f41e60442243cce566
eternity340 <2698182174@qq.com>
2024-06-19 15:30:14 +0800

    appRoute  revise

--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart' as getx;
-import '../page/get_email_code_page.dart';
+
+import '../email/page/get_email_code_page.dart';
+import '../email/page/verify_email_page.dart';
+import '../email/page/verify_success_page.dart';
 import '../pre_login/sign_up/sign_up_page.dart';
-import '../provider/get_email_code_provider.dart';
-import '../page/verify_email_page.dart';
-import '../provider/verify_email_provider.dart';
+import '../pre_login/welcome/welcome_page.dart';
+import '../pre_login/welcome/welcome_provider.dart';
+import '../email/provider/get_email_code_provider.dart';
+
+import '../email/provider/verify_email_provider.dart';
 import '../pre_login/sign_in/sign_in_page.dart';
 import '../pre_login/sign_in/sign_in_provider.dart';
 
-import '../page/verify_success_page.dart';
+
 
 class AppRoutes {
   static final routes = [
+    getx.GetPage(
+      name: '/welcome',
+      page: () => WelcomeProvider(
+        child: WelcomePage(),
+      ),
+    ),
     getx.GetPage(
       name: '/sign_in',
       page: () => SignInProvider(
@@ -44,7 +55,7 @@ class AppRoutes {
       },
     ),
     getx.GetPage(
-      name: '/sign_up', // 添加注册页面路由
+      name: '/sign_up',
       page: () => SignUpPage(),
     ),
   ];
