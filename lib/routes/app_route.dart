9e2a028530dd8b7f83263d641624e12b294d2e01
eternity340 <2698182174@qq.com>
2024-06-24 09:38:08 +0800

    route revise

--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -1,69 +1,86 @@
-  import 'package:flutter/material.dart';
-  import 'package:get/get.dart' as getx;
+import 'package:get/get.dart' as getx;
+import '../email/page/get_email_code_page.dart';
+import '../email/page/verify_email_page.dart';
+import '../email/page/verify_success_page.dart';
+import '../entity/User.dart';
+import '../pre_login/forget_pwd/forget_pwd_page.dart';
+import '../pre_login/forget_pwd/forget_pwd_provider.dart';
+import '../pre_login/sign_up/display_userinfo_page.dart';
+import '../pre_login/sign_up/select_birthday_page.dart';
+import '../pre_login/sign_up/select_gender_page.dart';
+import '../pre_login/sign_up/select_location_page.dart';
+import '../pre_login/sign_up/sign_up_page.dart';
+import '../pre_login/welcome/welcome_page.dart';
+import '../pre_login/welcome/welcome_provider.dart';
+import '../email/provider/get_email_code_provider.dart';
+import '../pre_login/sign_in/sign_in_page.dart';
+import '../pre_login/sign_in/sign_in_provider.dart';
 
-  import '../email/page/get_email_code_page.dart';
-  import '../email/page/verify_email_page.dart';
-  import '../email/page/verify_success_page.dart';
-  import '../pre_login/forget_pwd/forget_pwd_page.dart';
-  import '../pre_login/forget_pwd/forget_pwd_provider.dart';
-  import '../pre_login/sign_up/sign_up_page.dart';
-  import '../pre_login/welcome/welcome_page.dart';
-  import '../pre_login/welcome/welcome_provider.dart';
-  import '../email/provider/get_email_code_provider.dart';
+class AppRoutes {
+  static final routes = [
+    getx.GetPage(
+      name: '/welcome',
+      page: () => WelcomeProvider(
+        child: WelcomePage(),
+      ),
+    ),
+    getx.GetPage(
+      name: '/sign_in',
+      page: () => SignInProvider(
+        child: SignInPage(),
+      ),
+    ),
+    getx.GetPage(
+      name: '/get_mail_code',
+      page: () => GetEmailCodeProvider(
+        child: GetMailCodePage(),
+      ),
+    ),
+    getx.GetPage(name: '/verify_email', page: () {
+      final args = getx.Get.arguments as Map<String, dynamic>;
+      return VerifyEmailPage(
+        email: args['email'],
+        verificationKey: args['verificationKey'],
+      );
+    }),
+    getx.GetPage(name: '/verify_success', page: () {
+      final args = getx.Get.arguments as Map<String, dynamic>;
+      return VerifySuccessPage(
+        message: args['message'],
+        user: args['user'],
+      );
+    }),
 
-  import '../email/provider/verify_email_provider.dart';
-  import '../pre_login/sign_in/sign_in_page.dart';
-  import '../pre_login/sign_in/sign_in_provider.dart';
-
-
-  class AppRoutes {
-    static final routes = [
-      getx.GetPage(
-        name: '/welcome',
-        page: () => WelcomeProvider(
-          child: WelcomePage(),
-        ),
+    getx.GetPage(
+      name: '/forget_pwd',
+      page: () => const ForgetPwdProvider(
+        child: ForgetPwdPage(),
       ),
-      getx.GetPage(
-        name: '/sign_in',
-        page: () => SignInProvider(
-          child: SignInPage(),
-        ),
-      ),
-      getx.GetPage(
-        name: '/get_mail_code',
-        page: () => GetEmailCodeProvider(
-          child: GetMailCodePage(),
-        ),
-      ),
-      getx.GetPage(
-        name: '/verify_email',
-        page: () {
-          final args = getx.Get.arguments as Map<String, dynamic>;
-          return VerifyEmailPage(
-            email: args['email'],
-            verificationKey: args['verificationKey'],
-          );
-        },
-      ),
-      getx.GetPage(
-        name: '/verify_success',
-        page: () {
-          final args = getx.Get.arguments as Map<String, dynamic>;
-          return VerifySuccessPage(
-            message: args['message'] ?? '',
-          );
-        },
-      ),
-      getx.GetPage(
-        name: '/sign_up',
-        page: () => const SignUpPage(),
-      ),
-      getx.GetPage(
-        name: '/forget_pwd',
-        page: () => const ForgetPwdProvider(
-          child: ForgetPwdPage(),
-        ),
-      ),
-    ];
-  }
+    ),
+    getx.GetPage(name: '/select_gender', page: () {
+      final user = getx.Get.arguments as User;
+      return SelectGenderPage(user: user);
+    }),
+    getx.GetPage(name: '/select_location', page: () {
+      final user = getx.Get.arguments as User;
+      return SelectLocationPage(user: user);
+    }),
+    getx.GetPage(name: '/select_birthday', page: () {
+      final user = getx.Get.arguments as User;
+      return SelectBirthdayPage(user: user);
+    }),
+    getx.GetPage(
+      name: '/sign_up',
+      page: () {
+        final user = getx.Get.arguments as User;
+        return SignUpPage(user: user);
+      },
+    ),
+    getx.GetPage(
+      name: '/display_user_info',
+      page: () {
+        return DisplayUserInfoPage();
+      },
+    ),
+  ];
+}
