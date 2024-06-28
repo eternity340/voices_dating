4b6f762efc606caf8d8c74bf0901358a9496154d
eternity340 <2698182174@qq.com>
2024-06-28 08:46:49 +0800

    background color submit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF20E2D7);
-const kPrimaryLightColor = Color(0xFFD8FAAD);
+const kPrimaryLightColor = Color(0xFFFFFFFF);
 
 const double defaultPadding = 16.0;
